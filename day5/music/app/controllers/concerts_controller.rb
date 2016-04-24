1d39d6ac89601d8dc501ba50d320ace030a0ff54
Carlos Cardo <ccardo29@hotmail.com>
2016-04-24 08:53:20 -0400

    Music concert, list concert, add concert+CSS
    some CSS for the index page

--- day5/music/app/controllers/concerts_controller.rb
+++ day5/music/app/controllers/concerts_controller.rb
@@ -13,5 +13,18 @@ class ConcertsController < ApplicationController
 		@my_concert = Concert.new 
 		render "new"
 	end
-
+    def create
+    	@my_concert= Concert.new(
+			:artist => params[:concert][:artist],
+			:venue => params[:concert][:venue],
+			:city => params[:concert][:city],
+			:date => params[:concert][:date],
+            :price => params[:concert][:price],
+			:description => params[:concert][:description])
+         if @my_concert.save                      #no necesito salvar primero, basta solo con el condicional para q salve
+            redirect_to "/concerts"
+         else
+            render "new"
+         end
+    end
 end
